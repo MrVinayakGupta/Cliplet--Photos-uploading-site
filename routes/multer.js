513e881c780b4f90a8b538177f43bd4a9ec0fda9
MrVinayakGupta <163561684+MrVinayakGupta@users.noreply.github.com>
2026-03-15 23:01:40 +0530

    Update multer.js

---
+++ routes/multer.js
@@ -0,0 +1,14 @@
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        const uniqueFilename = uuidv4();
+        cd(null, uniqueFilename + '-' + file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
