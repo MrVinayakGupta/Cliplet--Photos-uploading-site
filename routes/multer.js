02f3b47f6e2c2368f9cbe04aa2c38284c228cc40
MrVinayakGupta <163561684+MrVinayakGupta@users.noreply.github.com>
2026-03-16 22:44:55 +0530

    adding path to store file

--- routes/multer.js
+++ routes/multer.js
@@ -1,13 +1,14 @@
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
+const path = require('path');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '/images/uploads');
+    cb(null, './public/images/uploads');
     },
     filename: function (req, file, cb) {
         const uniqueFilename = uuidv4();
-        cb(null, uniqueFilename);
+        cb(null, uniqueFilename + path.extname(file.originalname));
     }
 });
 
