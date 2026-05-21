f430c952a08bec2665c1856c4c7c07e46ff6c154
MrVinayakGupta <163561684+MrVinayakGupta@users.noreply.github.com>
2026-05-21 21:12:04 +0530

    Create users.js

---
+++ models/users.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require("passport-local-mongoose").default;
+
+mongoose.connect("mongodb://127.0.0.1:27017/PinterestCloneReplica");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+  },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post' // assuming you have a Post model
+    }
+  ],
+  dp: {
+    type: String, // store image URL or file path
+    default: ''
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    trim: true
+  },
+  fullName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  title: {
+    type: String,
+    trim: true
+  },
+  bio: {
+    type: String,
+    trim: true
+  },
+  followers: [
+    {
+       type: mongoose.Schema.Types.ObjectId, 
+       ref: 'User' 
+    }
+  ],
+  following: [
+    {
+       type: mongoose.Schema.Types.ObjectId, 
+       ref: 'User' 
+    }
+  ]
+}, { timestamps: true });
+
+userSchema.index({ username: "text", fullName: "text", bio: "text" });
+// ✅ Apply the plugin correctly
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', userSchema);
